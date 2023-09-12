5849025ca7270c81ea4e3f65f53a33e487a018b2
Ponsiano De Loor <ponsianodeloor@gmail.com>
2023-09-11 23:05:16 -0500

    Initial commit

---
+++ src/main/java/com/apptics/api/SpringAngularPostgresApplication.java
@@ -0,0 +1,13 @@
+package com.apptics.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularPostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAngularPostgresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apptics/api/SpringAngularPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apptics.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAngularPostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
